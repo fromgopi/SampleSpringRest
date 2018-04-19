056c6ec966f470eb876d985822c55c9a5b49a072
Gopi Krishna Muktevi <muktevigk@handbookinc.com>
2018-04-19 09:10:41 +0530

    Mongo config in Place and removed react part for now

---
+++ src/main/java/com/example/SimpleSpringBoot/config/ApplicationConfig.java
@@ -0,0 +1,14 @@
+package com.example.SimpleSpringBoot.config;
+
+import com.example.SimpleSpringBoot.beans.Names;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfig {
+
+    @Bean
+    public Names names(){
+        return new Names();
+    }
+}
