d43f4b81d09d8beefbb96c81ba450198788551ba
Gopi Krishna Muktevi <muktevigk@handbookinc.com>
2018-05-05 23:03:18 +0530

    Removed frontend part from repo for now

--- frontend/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import NameForm from './form';
-import registerServiceWorker from './registerServiceWorker';
-
-/*ReactDOM.render(<App />, document.getElementById('root'));*/
-ReactDOM.render(<NameForm />, document.getElementById('root'));
-registerServiceWorker();
